8659ee7c2d34e4ebcf454b6a86ff8178e9ed3e17
Rose1306 <rose.niang13@gmail.com>
2022-10-25 20:33:30 +0200

    Ajout des testSample

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -3,6 +3,9 @@ package fr.lernejo;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static fr.lernejo.Sample.Operation.ADD;
+import static fr.lernejo.Sample.Operation.MULT;
+
 class SampleTest {
 
     private final Sample sample = new Sample();
@@ -15,4 +18,30 @@ class SampleTest {
             .as("Addition of 2 and 2")
             .isEqualTo(4);
     }
+
+
+
+        @Test
+        void testADD() {
+            Sample sample = new Sample();
+            org.junit.jupiter.api.Assertions.assertEquals(sample.op(ADD, 2, 2), 4);
+        }
+
+        @Test
+        void testMULT() {
+            Sample sample = new Sample();
+            org.junit.jupiter.api.Assertions.assertEquals(sample.op(MULT, 2, 2), 4);
+        }
+
+        @Test
+        void testNullFact() {
+            Sample sample = new Sample();
+            org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> sample.fact(-5));
+        }
+
+        @Test
+        void testFactIfZero() {
+            Sample sample = new Sample();
+            org.junit.jupiter.api.Assertions.assertEquals(sample.fact(0), 1);
+        }
 }
